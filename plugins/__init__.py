de67b20463053df6bc50b12810a54d1657c0f388
DeveloperTharun <114932079+DeveloperTharun@users.noreply.github.com>
2023-11-29 21:45:45 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -10,13 +10,13 @@ TO_CHATS = set(int(x) for x in os.environ.get("TO_CHATS", "").split())
 OMDB_KEY = environ.get("OMDB_KEY", "")
 
 RE1TXT = os.environ.get("RE1TXT", "https://player.mrfooll.xyz/watch")
-RE2TXT = os.environ.get("RE2TXT", "@Username1")
+RE2TXT = os.environ.get("RE2TXT", "https://player.mrfooll.xyz/watch/400910/file.mkv?hash=AgADjA")
 RE3TXT = os.environ.get("RE3TXT", "⚠️ Uploaded By @HQFilms4U")
 RE4TXT = os.environ.get("RE4TXT", "@Username3")
 RE5TXT = os.environ.get("RE5TXT", "@Username4")
 RE6TXT = os.environ.get("RE6TXT", "@Username5")
 
 # text you want to replace with text above.
-REPLACED = os.environ.get("REPLACED", "@Hollywood_0980")
+REPLACED = os.environ.get("REPLACED", "")
 
 # MEDIA_FORWARD_ID = int(os.environ.get("MEDIA_FORWARD_ID"))
